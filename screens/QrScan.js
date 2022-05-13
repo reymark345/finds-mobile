c2aa010da52fad77ce3a9c297c5423c43e2b2530
reymark <reymarkvaldehueza@gmail.com>
2022-05-14 01:03:44 +0800

    feature/87:QR CODE

--- screens/QrScan.js
+++ screens/QrScan.js
@@ -2,29 +2,20 @@ import React, { Component, Fragment, useState } from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import styles from './scanStyle'
 import {
-  TouchableOpacity,
   Text,
-  StatusBar,
   View,
   Image,
   ScrollView,
   SafeAreaView
 } from 'react-native';
 
-import {
-  Header,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-import { HeaderBar, TevLabel, TextButton, TransactionHistory } from "../components"
-import { COLORS, SIZES, FONTS } from '../constants';
+import { HeaderBar,TextButton} from "../components"
+import {COLORS, SIZES} from '../constants';
 
-const QrScan = ({ navigation }) => {
+const QrScan = () => {
   const [scan, setScan] = useState(false);
   const [ScanResult, setScanResult] = useState(false);
   const [result, setResult] = useState(null);
-  const desccription = 'Financially Integrated Dynamic System'
-  const [selectedTev, setSelectedTev] = React.useState(null)
-
 
   function renderData() {
     return (
@@ -38,84 +29,80 @@ const QrScan = ({ navigation }) => {
           ...styles.shadow
         }}
       >
-        {(scan && (
-          <QRCodeScanner
-            reactivate={true}
-            showMarker={true}
-            onRead={onSuccess}
-            cameraContainerStyle={{ width: 275, borderWidth: 1, borderColor: 'white', alignSelf: 'center', }} cameraStyle={{ width: '97%', alignSelf: 'center', }}
-            // cameraStyle={[{ height: 300 }]}
-            // topContent={
-            //   <Text style={styles.centerText}>
-            //     Scan a QR Code</Text>
-            // }
-            bottomContent={
-              <View style={{
-                padding: SIZES.padding,
-                alignItems: "center",
-                justifyContent: "center"
-              }}>
-              </View>
-            }
-          />))
-
-          || (
-            <View >
-              <Image
-                source={require('../assets/images/qrcode_logo.png')}
-                style={{ padding: 30, height: 350, width: 300 }}
-              />
-            </View>
-          )}
-        {ScanResult &&
-          <Fragment>
-            <Text style={styles.textTitle1}>Result !</Text>
-            <View style={ScanResult ? styles.scanCardView : styles.cardView}>
-              <Text>Type : {result.type}</Text>
-              <Text>Result : {result.data}</Text>
-              <Text numberOfLines={1}>RawData: {result.rawData}</Text>
-              <TouchableOpacity onPress={scanAgain} style={styles.buttonTouchable}>
-                <Text style={styles.buttonTextStyle}>Retry</Text>
-              </TouchableOpacity>
-            </View>
-          </Fragment>
+        {scan && 
+        <View>
+        <QRCodeScanner
+        reactivate={true}
+        showMarker={true}
+        onRead={onSuccess}
+        cameraContainerStyle={{ width: 275, borderWidth: 1, borderColor: 'white', alignSelf: 'center', }} cameraStyle={{ width: '97%', alignSelf: 'center', }}
+        bottomContent={
+          <View style={{
+            padding: SIZES.padding,
+            alignItems: "center",
+            justifyContent: "center"
+          }}>
+          </View>
         }
-        {(scan && (
+      />
+      <View>
+        <TextButton
+          style={{
+            alignItems: 'center',
+          }}
+          height={50}
+          label="STOP SCAN"
+          onPress={onStop} />
+      </View>
+      </View>
+      }
+        {ScanResult &&
           <View>
-            <TextButton
-              style={{
-                alignItems: 'center',
-              }}
-              height={50}
-              label="STOP SCAN"
-              onPress={onStop} />
-          </View>)) || (
-            <View>
+            <Text style={styles.textTitle1}>Result !</Text>
+            <Text>Type : {result.type}</Text>
+            <Text>Result : {result.data}</Text>
+            <Text numberOfLines={1}>RawData: {result.rawData}</Text>
+            <View style={{marginTop:20}}>
               <TextButton
                 style={{
                   alignItems: 'center',
                 }}
                 height={50}
-                label="SCAN QR Code"
-                onPress={() => setScan(true)} />
+                label="RETRY"
+                onPress={scanAgain} />
             </View>
-          )}
-
-      </View>
-
-
+          </View>
+        }                                   
+        {!scan && !ScanResult &&
+          <View >
+            <Image
+              source={require('../assets/images/qrcode_logo.png')}
+              style={{ padding: 30, height: 350, width: 300 }}
+            />
+          <View>
+          <TextButton
+            style={{
+              alignItems: 'center',
+            }}
+            height={50}
+            label="Scan QR Code"
+            onPress={() => setScan(true)} />
+        </View>
+          </View>
+        }
+  </View>
     )
   }
 
   onStop = () => {
     setScan(false);
+    setScanResult(false);
 
   }
 
   onSuccess = (e) => {
     const check = e.data.substring(0, 4);
     console.log('scanned data' + check);
-
     setResult(e);
     setScan(false);
     setScanResult(true);
@@ -139,8 +126,6 @@ const QrScan = ({ navigation }) => {
   scanAgain = () => {
     setScan(true);
     setScanResult(false);
-
-    console.log("Pressed");
   }
 
   return (
@@ -154,62 +139,8 @@ const QrScan = ({ navigation }) => {
           {renderData()}
 
         </View>
-
-
       </ScrollView>
     </SafeAreaView>
-    // <View style={styles.scrollViewStyle}>
-    //   <Fragment>
-    //     <StatusBar barStyle="dark-content" />
-    //     <Text style={styles.textTitle}>FINDS</Text>
-    //     {!scan && !ScanResult &&
-    //       <View style={styles.cardView} >
-    //         <Text numberOfLines={8} style={styles.descText}>{desccription}</Text>
-    //         <TouchableOpacity onPress={activeQR} style={styles.buttonTouchable}>
-    //           <Text style={styles.buttonTextStyle}>Click to Scan !</Text>
-    //         </TouchableOpacity>
-
-    //       </View>
-    //     }
-
-    //     {ScanResult &&
-    //       <Fragment>
-    //         <Text style={styles.textTitle1}>Result !</Text>
-    //         <View style={ScanResult ? styles.scanCardView : styles.cardView}>
-    //           <Text>Type : {result.type}</Text>
-    //           <Text>Result : {result.data}</Text>
-    //           <Text numberOfLines={1}>RawData: {result.rawData}</Text>
-    //           <TouchableOpacity onPress={scanAgain} style={styles.buttonTouchable}>
-    //             <Text style={styles.buttonTextStyle}>Retry</Text>
-    //           </TouchableOpacity>
-
-    //         </View>
-    //       </Fragment>
-    //     }
-    //     {scan &&
-    //       <QRCodeScanner
-    //         reactivate={true}
-    //         showMarker={true}
-    //         ref={(node) => { scanner = node }}
-    //         onRead={onSuccess}
-    //         topContent={
-    //           <Text style={styles.centerText}>
-    //             Scan a QR Code</Text>
-    //         }
-    //         bottomContent={
-    //           <View>
-    //             <TouchableOpacity style={styles.buttonTouchable} onPress={() => setScan(false)}>
-    //               <Text style={styles.buttonTextStyle}>Stop Scan</Text>
-    //             </TouchableOpacity>
-    //           </View>
-
-    //         }
-    //       />
-    //     }
-    //   </Fragment>
-    // </View>
   );
 }
-
-
 export default QrScan;
